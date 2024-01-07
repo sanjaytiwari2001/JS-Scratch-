7f0a2b1b971f0c94a0ca11c5fe26fc861cde1121
Sanjay Tiwari <131176592+sanjaytiwari2001@users.noreply.github.com>
2024-01-07 16:52:10 +0000

    Data Type

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 14453
+let accountEmail = "sanjay@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accounId=2  // not allowed 
+
+accountEmail="hthth@.com"
+accountPassword = "412"
+accountCity ="bihar"
+
+console.log(accountId);
+
+/* 
+prefer not to use var
+ because of issue in block scope and  functional scope
+*/
+
+console.table([accountId, accountEmail ,  accountPassword , accountCity , accountState]);
+
+
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,40 @@
+"use strict"; // treat all JS code as newr version 
+
+ //alert("Hello")  we are using nodeJs , not browser
+
+console.log(3 
+    +
+    3) // code readability should be high 
+
+
+    console.log("hitesh")
+
+
+    // premitive dataType 
+
+    // number
+    // bigint 
+    // string ==> ""
+    // boolean ==> true / false
+    // null => Standalone value
+    // undefined ==>
+    // symbol ==> unique 
+
+    // object
+    
+    let age = 1234;
+
+
+    // Typeof 
+    console.log(typeof null); // object somthing error of JS 
+    console.log(typeof 'sanjay'); // string 
+    // console.log(typeof 123);
+    // console.log(typeof age);
+    // console.log(typeof undefined);
+    console.log(typeof age);
+    console.log(typeof 123);
+
+
+
+    
+
